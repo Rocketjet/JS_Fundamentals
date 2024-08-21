c5b80fc1bfaa06269d1e98e9ca31649307c29f8b
Maksym Vikolash <vikolashmaxim@gmail.com>
2024-08-21 23:38:47 +0300

    updated Promise examples

--- Promise/Promise.all.js
+++ Promise/Promise.all.js
@@ -3,27 +3,40 @@
 var p1 = Promise.resolve(3);
 var p2 = 1337;
 var p3 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 100, "foo");
+  setTimeout(resolve, 100, 'foo');
 });
 
-Promise.all([p1, p2, p3]).then(values => {
+Promise.all([p1, p2, p3]).then((values) => {
   console.log(values);
 });
 
-//Выведет:
-// [3, 1337, "foo"] 
+// [3, 1337, "foo"]
 
-function reflect(promise) {
+function resolver(promise) {
   return promise.then(
     (v) => {
       return { status: 'fulfilled', value: v };
     },
     (error) => {
       return { status: 'rejected', reason: error };
-    }
+    },
   );
 }
 
-const promises = [ fetch('index.html'), fetch('https://does-not-exist/') ];
-const results = await Promise.all(promises.map(reflect));
-const successfulPromises = results.filter(p => p.status === 'fulfilled');
+let dataFromPromise;
+const promises = [fetch('https://api.sampleapis.com/coffee/hot'), fetch('https://api.sampleapis.com/coffee/hot')];
+
+const results = Promise.all(promises)
+  .then((resp) => {
+    return resp.map((p) => p.json());
+  })
+  .then((data) => {
+    return Promise.all(data.map(resolver));
+  })
+  .then((drinks) => {
+    dataExtractor(drinks.map((d) => d.value).flat());
+  });
+
+function dataExtractor(data) {
+  dataFromPromise = data;
+}
--- Promise/createPromise.js
+++ Promise/createPromise.js
@@ -1,29 +1,32 @@
-
 function createPromise() {
+  console.log('createPromise FIRED');
   const completeButton = document.getElementById('complete');
   const cancelButton = document.getElementById('cancel');
 
   const resolver = (resolve, reject) => {
+    console.log('Resolver fn FIRED');
     completeButton.addEventListener('click', () => {
       resolve('Nice');
-      console.log(resolve);
     });
     cancelButton.addEventListener('click', () => {
       reject('Bad');
-      console.log(reject);
     });
-  }
+  };
 
   return new Promise(resolver);
 }
+
+setTimeout(() => {
+  console.log('___START___');
+}, 150);
 
 const promise = createPromise();
 console.log(promise);
 
 promise
-  .then(result => (
-    console.log('Success is ', result)
-  ))
-  .catch(error => {
+  .then((result) => console.log('Success is ', result))
+  .catch((error) => {
     console.log('Failure is', error);
-  })
+  });
+
+console.log('___DONE___');
